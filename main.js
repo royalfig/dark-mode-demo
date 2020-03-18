d7a17876471c402d8ee6db00c29101b146d93fa6
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2020-03-18 16:09:39 -0400

    First

---
+++ main.js
@@ -0,0 +1,19 @@
+const root = document.documentElement;
+root.setAttribute("color-mode", "light");
+
+// Define a function to
+const toggleColorMode = e => {
+  if (e.currentTarget.classList.contains("light")) {
+    root.setAttribute("color-mode", "light");
+    localStorage.setItem("color-pref", "light");
+    return;
+  }
+  root.setAttribute("color-mode", "dark");
+  localStorage.setItem("color-pref", "dark");
+};
+
+const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
+
+toggleColorButtons.forEach(btn => {
+  btn.addEventListener("click", toggleColorMode);
+});
