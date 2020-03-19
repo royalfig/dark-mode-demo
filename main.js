201e958f32f8026c62d4952becd38f348e6ee10d
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2020-03-19 19:29:24 -0400

    updated classes to accord with tutorial

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-const root = document.documentElement;
-
-// Define a function to
 const toggleColorMode = e => {
-  if (e.currentTarget.classList.contains("light")) {
-    root.setAttribute("color-mode", "light");
+  // Switch to Light Mode
+  if (e.currentTarget.classList.contains("light--hidden")) {
+    // Sets the custom html attribute
+    document.documentElement.setAttribute("color-mode", "light");
+
+    // Sets the user's preference in local storage
     localStorage.setItem("color-mode", "light");
     return;
   }
-  root.setAttribute("color-mode", "dark");
+
+  /* Switch to Dark Mode
+  Sets the custom html attribute */
+  document.documentElement.setAttribute("color-mode", "dark");
+
+  // Sets the user's preference in local storage
   localStorage.setItem("color-mode", "dark");
 };
 
+// Get the buttons in the DOM
 const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
 
+// Set up event listeners
 toggleColorButtons.forEach(btn => {
   btn.addEventListener("click", toggleColorMode);
 });
